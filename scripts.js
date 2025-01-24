73de8b610a21a8540fadd60ba42d60b930ed7009
ea <enochadediran@ea-2.local>
2025-01-24 04:24:31 +0100

    Initial commit for  Boy website

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+/* JavaScript for $RICH Boy Token website */
+
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
